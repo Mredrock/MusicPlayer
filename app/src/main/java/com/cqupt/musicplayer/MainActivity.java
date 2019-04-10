9358724698cd598f73b454a50766b6a0810d930a
Mredrock <midsummercn@live.cn>
2019-04-10 11:39:45 +0800

    基本布局做好，准备切换为限制布局，做个备份

--- app/src/main/java/com/cqupt/musicplayer/MainActivity.java
+++ app/src/main/java/com/cqupt/musicplayer/MainActivity.java
@@ -1,23 +1,56 @@
 package com.cqupt.musicplayer;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
     ListView listView;
-
+    private ArrayList<String> list = new ArrayList<String>();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         listView = (ListView)findViewById(R.id.list_view);
-        String[] strArr = new String[] { "姜必群 - 围棋少年.mp3", "张杰,张碧晨 - 只要平凡.mp3",
-                "张靓颖 - 生如夏花 (Live).mp3" };
+        list.add( "姜必群 - 围棋少年.mp3");
+        list.add("张杰,张碧晨 - 只要平凡.mp3");
+        list.add("张靓颖 - 生如夏花 (Live).mp3");
+
         listView = (ListView) findViewById(R.id.list_view);
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
-                MainActivity.this, android.R.layout.simple_list_item_1, strArr);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_list_item_1, list);
         listView.setAdapter(adapter);
+
+
+        //获取当前ListView点击的行数，并且得到该数据
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                switch (list.get(position)){
+                    case "姜必群 - 围棋少年.mp3":
+                        Intent intent0 = new Intent(MainActivity.this, PlayActivity.class);
+                        intent0.putExtra("name","a");
+                        startActivity(intent0);
+                        break;
+                    case "张杰,张碧晨 - 只要平凡.mp3":
+                        Intent intent1 = new Intent(MainActivity.this, PlayActivity.class);
+                        intent1.putExtra("name","b");
+                        startActivity(intent1);
+                        break;
+                    case "张靓颖 - 生如夏花 (Live).mp3":
+                        Intent intent2 = new Intent(MainActivity.this, PlayActivity.class);
+                        intent2.putExtra("name","c");
+                        startActivity(intent2);
+                        break;
+                }
+
+            }
+        });
     }
+
 }
---
+++ app/src/main/java/com/cqupt/musicplayer/PlayActivity.java
@@ -0,0 +1,132 @@
+package com.cqupt.musicplayer;
+
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.SeekBar;
+import android.widget.TextView;
+
+import java.util.concurrent.TimeUnit;
+
+public class PlayActivity extends AppCompatActivity {
+
+    private MediaPlayer mediaPlayer;
+    private TextView tv_start;
+    private TextView tv_end;
+    private SeekBar seekbar;
+    String filename;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_play);
+        Intent intent = getIntent();
+        filename = intent.getStringExtra("name");
+        //开始时间
+        tv_start = (TextView) findViewById(R.id.tv_start);
+        //结束时间
+        tv_end = (TextView) findViewById(R.id.tv_end);
+        //进度条
+        seekbar = (SeekBar) findViewById(R.id.seekbar);
+        //设置监听
+        seekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                //获取音乐总时间
+                int duration2=mediaPlayer.getDuration()/1000;
+                //获取音乐当前播放的位置
+                int position=mediaPlayer.getCurrentPosition();
+                //开始时间
+                tv_start.setText(calculateTime(position/1000));
+                //结束时间
+                tv_end.setText(calculateTime(duration2));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                int progress=seekBar.getProgress();
+                //在当前位置播放
+                mediaPlayer.seekTo(progress);
+            }
+        });
+
+    }
+
+    //按钮点击事件
+    public void isPlayOrPause(View view){
+        ImageButton imageButton= (ImageButton) view;
+        //判断音频文件是否为空
+        if(mediaPlayer==null){
+            //为空则创建音乐文件并播放改变按钮样式
+            if (filename == "a"){
+                mediaPlayer = MediaPlayer.create(this, R.raw.a);
+            }
+            else if (filename == "b"){
+                mediaPlayer = MediaPlayer.create(this, R.raw.b);
+            }
+            else if (filename == "c"){
+                mediaPlayer = MediaPlayer.create(this, R.raw.c);
+            }
+
+            mediaPlayer.start();
+            imageButton.setImageResource(android.R.drawable.ic_media_pause);
+            //获取音乐总时间
+            int duration=mediaPlayer.getDuration();
+            //将音乐总时间设置为SeekBar的最大值
+            seekbar.setMax(duration);
+            //线程修改时间值
+            new MyThread().start();
+            //音乐文件正在播放，则暂停并改变按钮样式
+        }else if(mediaPlayer.isPlaying()){
+            mediaPlayer.pause();
+            imageButton.setImageResource(android.R.drawable.ic_media_play);
+        }else{
+            //启动播放
+            mediaPlayer.start();
+            imageButton.setImageResource(android.R.drawable.ic_media_pause);
+        }
+    }
+
+    //使用线程控制进度条时间
+    class MyThread extends Thread{
+        @Override
+        public void run() {
+            super.run();
+            while(seekbar.getProgress()<=seekbar.getMax()){
+                //获取音乐当前播放的位置
+                int position=mediaPlayer.getCurrentPosition();
+                //放入SeekBar中
+                seekbar.setProgress(position);
+            }
+        }
+    }
+
+    //计算播放时间
+    public String calculateTime(int time){
+        int minute;
+        int second;
+        if(time>=60){
+            minute=time/60;
+            second=time%60;
+            return minute+":"+second;
+        }else if(time<60){
+            second=time;
+            return "0:"+second;
+        }
+        return null;
+    }
+
+
+}
+
